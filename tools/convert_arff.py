0fb3992f2d543e937359b84166c99c015bef7aad
Patrick Clerkin <patrick.code@fastmail.fm>
2016-09-13 16:18:41 +0100

    Added data handling

--- tools/convert_arff.py
+++ tools/convert_arff.py
@@ -2,13 +2,17 @@
 
 """
 This tool converts Weka ARFF files to python modules.
+
 See http://weka.wikispaces.com/ARFF+%28book+version%29
-This tool is designed to work with the ARFF files distributed with Weka version 3.6.14.
+
+It is designed to work with the ARFF files distributed with Weka version 3.6.14.
 It supports only numeric and nominal attributes; string, date and relation-valued attributes are not supported.
+It doesn't support missing values, represented in ARFF files by '?'.
 """
 
 import argparse
 import re
+import pprint
 
 class UnexpectedTokenException(Exception):
     pass
@@ -16,9 +20,12 @@ class UnexpectedTokenException(Exception):
 class UnsupportedAttributeException(Exception):
     pass
 
+class AttributeValueException(Exception):
+    pass
+
 class ArffParser(object):
 
-    RELATION, ATTRIBUTE, DATA = range(3)
+    RELATION, ATTRIBUTE, DATA, DATA_WRITTEN = range(4)
     PATTERN = '\{(.*)\}' # for matching attribute values
 
     def __init__(self):
@@ -26,6 +33,7 @@ class ArffParser(object):
         self.output = ''
         self.attributes = []
         self.values = []
+        self.data = []
 
     def handle_comment(self, line):
         line = '#' + line[1:]
@@ -54,7 +62,7 @@ class ArffParser(object):
         self.attributes.append(name)
         value = tokens[2]
         if value in ('numeric', 'REAL'):
-            self.values.append([])
+            self.values.append([]) # empty list means it's a float!
         elif value.startswith('{'):
             value_list = []
             values = re.findall(ArffParser.PATTERN, line)[0].split(',') 
@@ -65,23 +73,48 @@ class ArffParser(object):
         else:
             raise UnexpectedTokenException(tokens[0] + ', in line: ' + line)
 
+    def handle_data(self, line):
+        row = []
+        values = line.split(',')
+        if len(values) == len(self.attributes):
+            for i in range(len(values)):
+                value = values[i].strip()
+                if self.attributes[i] == []:
+                    try:
+                        row.append(float(value))
+                    except:
+                        raise AttributeValueException('Expected numeric value on line: ', + line)
+                else:
+                    row.append(value)
+            self.data.append(row)
+        else:
+            raise AttributeValueException('Wrong number on values on line: ', + line)
+
+    def output_data(self):
+        if self.expecting == ArffParser.DATA:
+            self.output += 'data = ' + pprint.pformat(self.data) + '\n'
+            self.expecting = ArffParser.DATA_WRITTEN
+
     def convert_line(self, line):
         line = line.lstrip()
         if line == '':
+            self.output_data()
             return
         if line[0] == '%':
-            line = self.handle_comment(line)
+            self.output_data()
+            self.handle_comment(line)
         elif self.expecting == ArffParser.RELATION:
-            line = self.handle_relation(line)
+            self.handle_relation(line)
         elif self.expecting == ArffParser.ATTRIBUTE:
-            line = self.handle_attribute(line)
+            self.handle_attribute(line)
         elif self.expecting == ArffParser.DATA:
-            pass
+            self.handle_data(line)
 
     def process(self, lines):
         output = ''
         for line in lines:
             line = self.convert_line(line)
+        self.output_data()
         return self.output
 
 if __name__ == '__main__':
